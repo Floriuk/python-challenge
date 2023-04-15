9e800ced8a4d0aaafb267c9c6d49819e1e801c82
Floriuk <salazarfloriuk@gmail.com>
2023-04-15 11:50:23 -0600

    Uploading the final version for the two parts of the challenge, python file, resources files, and the text files with the results

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,30 +1,68 @@
 import os, csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
+#declaring my variables
+sum_months = 0
+list_months = []
+list_diferences = []
+change = 0
+greatest_increase = 0
+greatest_increase_month = ""
+greatest_decrease = 0
+greatest_decrease_month = ""
+
+#open file as csv
 with open(csvpath, encoding='UTF-8') as csvfile:
     pointer = csv.reader(csvfile)
-    print('Financial Analysis')
-    print('------------------------')
+    # print('Financial Analysis')
+    # print('------------------------')
     next(pointer)
-    # Declaring my variables to analyze
+
     from statistics import mean
-    sum_months = 0
-    list_months = []
     current_value = 0
-    list_diferences = []
+
     for m in pointer:
         months = m[0]
         sum_months = sum_months + float(m[1])
         list_months.append(months)
+
         if current_value ==0:
             current_value = float(m[1])
         else:
+            
             list_diferences.append(float(m[1])-current_value)
-            current_value = float(m[1])
-        
+            
 
-print("Total Months: " + str(len(list_months)))
-print("Total: $", round(sum_months))
+            change = (float(m[1]) - current_value)
+            
+
+            if change > greatest_increase:
+                greatest_increase = change
+                greatest_increase_month = m[0]
+                
+
+            if change < greatest_decrease:
+                greatest_decrease = change
+                greatest_decrease_month = m[0]
+            
+            current_value = float(m[1])
+
+
 list_avg = mean(list_diferences)
-print("Average Change: $" + str(round(list_avg,2)))
-print('Greatest Increase in Profits: '+ m[0] + ' ($' + str(round(max(list_diferences)))+')')
-print('Greatest Decrease in Profits: '+ m[0] + ' ($' + str(round(min(list_diferences)))+')')
+
+#printing the output
+output = f"""
+Financial Analysis
+------------------------
+Total Months: {str(len(list_months))}
+Total: ${round(sum_months)}
+Average Change: ${str(round(list_avg,2))}
+Greatest Increase in Profits: {greatest_increase_month} (${str(round(greatest_increase))})
+Greatest Decrease in Profits: {greatest_decrease_month} (${str(round(greatest_decrease))})
+"""
+
+print(output)
+
+#creating text file with the results
+with open("analysis/pybank.text","w") as f:
+    f.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,61 @@
-import os, csv
-csvpath = os.path.join('Resources','election_data.csv')
+import os
+import csv
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+#opening file as csv
 with open(csvpath, encoding='UTF-8') as csvfile:
-    pointer = csv.reader (csvfile)
-    print('Election Results')
-    print(------------------------)
+    pointer = csv.reader(csvfile)
     next(pointer)
-    #Declaring my variables
+    # Declaring my variables
+    total_votes = 0
     sum_votes = 0
-    total_votes = []
+    candidate_dict = {}
+    candidate_list = []
 
+    #Creating the for loop to start retrieving info
     for x in pointer:
-        voters = x[0]
-        total_votes.append(voters)
+        total_votes += 1
+        candidate_name = x[2]
 
-print("Total votes: "+ str(len(total_votes)))
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+
+            candidate_dict[candidate_name] = 0
+
+        candidate_dict[candidate_name] +=1
+
+#creating my variablles to count the votes for each candidate
+charles_votes = candidate_dict["Charles Casper Stockham"]
+diana_votes = candidate_dict["Diana DeGette"]
+raymon_votes = candidate_dict["Raymon Anthony Doane"]
+charles_percentage = charles_votes/total_votes*100
+diana_percentage = diana_votes/total_votes*100
+raymon_percentage = raymon_votes/total_votes*100
+
+#comparing the votes of each candidate
+if charles_percentage > diana_percentage and charles_percentage > raymon_percentage:
+    winner = "Charles Casper Stockham"
+if diana_percentage > charles_percentage and diana_percentage > raymon_percentage:
+    winner = "Diana DeGette"
+if raymon_percentage > charles_percentage and raymon_percentage > diana_percentage:
+    winner = "Raymon Anthony Doane"
+    
+#printing results
+output = f"""
+Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------
+Charles Casper Stockham: {charles_percentage:.3f}% ({charles_votes})
+Diana DeGette: {diana_percentage:.3f}% ({diana_votes})
+Raymon Anthony Doane: {raymon_percentage:.3f}% ({raymon_votes})
+-------------------------
+Winner: {winner}
+-------------------------
+"""
+
+print(output)
+
+#exporting to text file
+with open("analysis/pypoll.text", "w") as f:
+    f.write(output)
